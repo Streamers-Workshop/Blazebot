76f8587be1608258b7f73084733bd370166d96dd
ssrjazz <ssrjazz@gmail.com>
2020-07-04 20:35:29 -0500

    Adding a shoutout (so) plugin file to new_branch

---
+++ plugins/so.js
@@ -0,0 +1,9 @@
+console.log('loaded so')
+module.exports = {
+	name: 'so',
+	description: 'Replies with a shoutout',
+	credits: 'Created by ssrjazz',
+	execute(message, args, user, bot) {
+		bot.sendMessage(`Checkout @${args} at https://trovo.live/${args} - They are an awesome streamer and deserve some community love! (TrovoBot)`);
+	},
+};
