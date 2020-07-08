10ab0d93fab498a45582c62c9de9f9c4c4eb1608
krammy <keenandavidmoloney@outlook.com>
2020-07-08 02:46:51 +0100

    Updated SO to work with and without @ character

--- plugins/so.js
+++ plugins/so.js
@@ -1,3 +1,16 @@
+/*
+PLUGIN BY: ssrjazz
+Trovo Link:
+Discord: ssrjazz#4929
+
+Description: Shoutout command
+
+
+Updated by: Krammy
+Changes: Added funcitonality for @ and non-@ shout outs.
+*/
+
+
 console.log('loaded so')
 module.exports = {
 	name: 'so',
@@ -5,6 +18,19 @@ module.exports = {
 	credits: 'Created by ssrjazz modified by bioblaze to make it only select the first arg in the args array <3 also added a perm check.',
 	execute(message, args, user, bot, event) {
 		if (event.badges == undefined || (event.badges.indexOf("moderator") <= -1 && event.badges.indexOf("creator") <= -1)) return;
-		bot.sendMessage(`Check out @${args[0]} at https://trovo.live/${args[0]} - They are an awesome streamer and deserve some community love! (TrovoBot)`);
+		var soUsername = args[0];
+		
+		if (soUsername.charAt(0) == '@')
+		{
+			bot.sendMessage(`Check out ${args[0]} at https://trovo.live/${args[0].substr(1)} - They are an awesome streamer and deserve some community love! (TrovoBot)`);
+		}
+		else
+		{
+			bot.sendMessage(`Check out @${args[0]} at https://trovo.live/${args[0]} - They are an awesome streamer and deserve some community love! (TrovoBot)`);
+
+		}
+		
+		
+		
 	},
 };
