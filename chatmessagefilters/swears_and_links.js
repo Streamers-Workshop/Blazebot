5f6f62ee07c27dd69a8d885cefc041fdaf8d7835
Takkes <robin.drangmeister@outlook.com>
2020-07-08 21:06:08 +0200

    Added @Ulashs bad word and link filter to new chat message filter system

---
+++ chatmessagefilters/swears_and_links.js
@@ -0,0 +1,25 @@
+let bJson = JSON.parse(fs.readFileSync("./chatmessagefilters/bannedWords.json", "utf8"));
+const bst = bJson.data[0];
+const bWords = bst.words[process.env.BADWORDS_LANG];
+const bLinks = bst.links;
+
+module.exports = {
+  name: 'badwords',
+  excludeBadges: ['moderator', 'creator'],
+  description: 'Deletes messages that contain bad words',
+  execute(message, bot, plugins, settings) {
+
+    if (!settings.BADWORDS || settings.BADWORDS === 0)
+      return false;
+
+    if (bLinks.some(word => message.content.includes(word))) {
+      bot.sendMessage(`@${message.user} Please don\'t make advertise :) !`);
+      setTimeout(() => bot.sendMessage(`/ban @${message.user} 10`), 1500);
+      return true;
+    } else if (bWords.some(word => message.content.includes(word))) {
+      bot.sendMessage(`@${message.user} Please be respectful in the chat!`);
+      setTimeout(() => bot.sendMessage(`/ban @${message.user} 10`), 1500);
+      return true;
+    }
+  }
+};
