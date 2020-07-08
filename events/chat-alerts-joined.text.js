1071604e3a6cbb47fdb5d333200eb84a1af912cc
Murat Ulaş ÖZTÜRK <53235368+CrackerFX@users.noreply.github.com>
2020-07-09 02:57:24 +0300

    Now more user friendly. Changed switch to JSON

--- events/chat-alerts-joined.text.js
+++ events/chat-alerts-joined.text.js
@@ -4,14 +4,13 @@ module.exports = {
 	event: 5004,
 	description: 'Welcome\'s a User who has Joined the Stream!',
 	execute(data, bot, plugins) {
+		let sJson = JSON.parse(fs.readFileSync("./events/specialMessages.json", "utf8"));
+		const special = sJson.specials[0];
 		if (users.indexOf(data.user) > -1) return;
 		users.push(data.user);
-		switch (data.user) {
-			case 'someone':
-				bot.sendMessage(`Ladies and gentlemen, tonight we have @${data.user} in the sky, and on stage. We are favored to welcome awesome person the world has seen.`);
-				break;
-			default:
-			bot.sendMessage(`Welcome @${data.user} Remember to Follow the Stream, and for visiting <3 type !ping or !pong to test commands!`);
+		if (special.users[data.user] != undefined) {
+				bot.sendMessage(special.users[data.user]);
 		}
+		else return bot.sendMessage(`Welcome @${data.user} Remember to Follow the Stream, and for visiting <3 type !ping or !pong to test commands!`);
 	},
 };
