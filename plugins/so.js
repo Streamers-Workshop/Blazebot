568b48e8554b8e77a3cb3f05a1ab109881fd8099
Bioblaze Payne <BioblazePayne@gmail.com>
2020-07-08 02:59:00 -0700

    Fixed Merge Conflicts my bad.

--- plugins/so.js
+++ plugins/so.js
@@ -16,15 +16,10 @@ module.exports = {
 	name: 'so',
 	description: 'Replies with a shoutout',
 	credits: 'Created by ssrjazz modified by bioblaze to make it only select the first arg in the args array <3 also added a perm check.',
-<<<<<<< HEAD
-  permissions: ['moderator', 'creator'],
+	permissions: ['moderator', 'creator'],
 	execute(message, args, user, bot, event, plugins) {
-		bot.sendMessage(`Check out @${args[0]} at https://trovo.live/${args[0]} - They are an awesome streamer and deserve some community love! (TrovoBot)`);
-=======
-	execute(message, args, user, bot, event) {
-		if (event.badges == undefined || (event.badges.indexOf("moderator") <= -1 && event.badges.indexOf("creator") <= -1)) return;
 		var soUsername = args[0];
-		
+
 		if (soUsername.charAt(0) == '@')
 		{
 			bot.sendMessage(`Check out ${args[0]} at https://trovo.live/${args[0].substr(1)} - They are an awesome streamer and deserve some community love! (TrovoBot)`);
@@ -34,9 +29,5 @@ module.exports = {
 			bot.sendMessage(`Check out @${args[0]} at https://trovo.live/${args[0]} - They are an awesome streamer and deserve some community love! (TrovoBot)`);
 
 		}
-		
-		
-		
->>>>>>> b5b0a8506fa9b6a6c0490a6e2e981f2dc0a34c09
 	},
 };
