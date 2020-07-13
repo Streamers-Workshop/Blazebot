5b4d6ad477e5988198f5235a198d96a7d55c9229
Bioblaze Payne <BioblazePayne@gmail.com>
2020-07-12 18:30:00 -0700

    Remove all unwanted files.

--- events/chat-alerts-subbed.text.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'chat-spell-event',
-	event: 5001,
-	description: 'Thanks a user for Subbing to the Channel.',
-	execute(data, bot, plugins) {
-    bot.sendMessage(`Thanks @${data.user} for subbing too the channel your amazing <3`);
-	},
-};
--- plugins/stats.README.md
+++
@@ -1,3 +0,0 @@
-In order to pull your stats you will need to edit the stats.Settings.json file to your own details
-platform acceptable options are: atvi = activision ID psn = ps4 xbl = xbox live battlenet = battlenet
-playerName = your player name
