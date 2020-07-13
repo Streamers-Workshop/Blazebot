62eaeacdcc3b83665d907188c46deab5f351e4d5
Takkes <robin.drangmeister@outlook.com>
2020-07-14 01:07:16 +0200

    Fix style of plugins

--- plugins/urbandictionary/urbandictionary.js
+++ plugins/urbandictionary/urbandictionary.js
@@ -1,8 +1,8 @@
 const https = require('https');
+
 module.exports = {
   name: 'Urban Dictionary',
   description: 'slang dictionary and for some meme :)',
-  permissions: [],
   chat: true, // Defines this as a Chat Command
   event: false, // Is this a Event?
   type: 5004, // Type Event
@@ -11,40 +11,40 @@ module.exports = {
   alias: [], // Alias commands that preform interesting things.
   cooldown: 1, // this is Set in Seconds, how long between the next usage of this command.
   settings: false, // Defining this as false will load the Settings file for this Plugin when the system loads this plugin.
-  credits: "Created by Rehkloos",
-  execute(client, data, modules) {
+  credits: 'Created by Rehkloos',
+  execute(client, data) {
     if (!data.args[0]) {
-      client.sendMessage("Example Usage: = " + `!ud <term>`);
+      client.sendMessage(`Example Usage: = !ud <term>`);
     } else if (data.args[0]) {
       const searchTerm = data.args[0];
-      const userUrl = 'https://api.urbandictionary.com/v0/define?term=' + searchTerm;
-      https.get(userUrl, (resp) => {
-        let ud = '';
+      const userUrl = `https://api.urbandictionary.com/v0/define?term=${searchTerm}`;
+      https
+        .get(userUrl, (resp) => {
+          let ud = '';
 
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-          ud += chunk;
-        })
-        resp.on('end', () => {
-          const obj = JSON.parse(ud);
-          if (obj) {
-            const udl = obj.list[0];
-            const word = udl.word;
-            const def = udl.definition;
-            client.sendMessage(`@${data.user} ${word} - ${def}`);
-          } else {
-            const err = obj.errors[0];
-            const errMsg = err.message;
-            if (errMsg) {
-              client.sendMessage("We could not find the term " + `${data.args[0]}` + "")
+          // A chunk of data has been recieved.
+          resp.on('data', (chunk) => {
+            ud += chunk;
+          });
+          resp.on('end', () => {
+            const obj = JSON.parse(ud);
+            if (obj) {
+              const udl = obj.list[0];
+              const { word } = udl;
+              const def = udl.definition;
+              client.sendMessage(`@${data.user} ${word} - ${def}`);
+            } else {
+              const err = obj.errors[0];
+              const errMsg = err.message;
+              if (errMsg) {
+                client.sendMessage(`We could not find the term ${data.args[0]}`);
+              }
             }
-          }
+          });
+        })
+        .on('error', (err) => {
+          console.log(`Error: ${err.message}`);
         });
-      }).on("error", (err) => {
-        console.log("Error: " + err.message);
-      });
-
-
     }
   },
 };
