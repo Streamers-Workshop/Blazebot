87874a29d7928ba829c8dcd0cf1aca1c6357715f
albertoparras-dev <66566819+albertoparras-dev@users.noreply.github.com>
2020-07-14 02:52:03 +0200

    Linted Plugins Table

--- modules/Plugins.js
+++ modules/Plugins.js
@@ -1,11 +1,12 @@
-const fs = require('fs'), path = require('path'), util = require('util');
+const fs = require('fs');
+const path = require('path');
 
-var instance = null;
+var instance = null; // eslint-disable-line no-var
 
-var AsciiTable = require('ascii-table')
+const AsciiTable = require('ascii-table');
 
 function Plugins() {
-  if(arguments.callee._singletonInstance) {
+  if (arguments.callee._singletonInstance) {
     return arguments.callee._singletonInstance;
   }
   arguments.callee._singletonInstance = this;
@@ -13,25 +14,30 @@ function Plugins() {
   this.chat = new Map();
 }
 
-let pluginsTable = new AsciiTable('PLUGINS');
-pluginsTable.setHeading('COMMAND', 'STATE')
+const pluginsTable = new AsciiTable('PLUGINS');
+pluginsTable.setHeading('COMMAND', 'STATE');
 
-const getDirectories = source => fs.readdirSync(source, { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name)
+const getDirectories = (source) =>
+  fs
+    .readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
 
 Plugins.prototype.loadPlugins = async (directory) => {
-  fs.access(directory, function(err) {
-    if (err) new Error(`File does not Exist or has Access Permission Issues: ${file}`);
+  fs.access(directory, (err) => {
+    if (err) throw new Error(`File does not Exist or has Access Permission Issues: ${err}`);
     else {
       const directories = getDirectories(directory);
-      for(const dir of directories) {
+      for (const dir of directories) {
         try {
-          var plugin = require(path.join(directory, dir, `${dir}.js`));
-          /** console.log(`Loaded Plugin[${plugin.name}] from File (/plugins/${dir}/${dir}.js)`); */
+          const plugin = require(path.join(directory, dir, `${dir}.js`));
           pluginsTable.addRow(plugin.name, 'LOADED');
           if (plugin.settings) {
             if (fs.accessSync(path.join(directory, dir, `${dir}.json`), fs.constants.R_OK)) {
               plugin.settings = require(path.join(directory, dir, `${dir}.json`));
-              console.log(`Loaded Plugin Settings[${plugin.name}] from File (/plugins/${dir}/${dir}.json)`);
+              console.log(
+                `Loaded Plugin Settings[${plugin.name}] from File (/plugins/${dir}/${dir}.json)`,
+              );
             } else {
               plugin.settings = null;
             }
@@ -40,48 +46,49 @@ Plugins.prototype.loadPlugins = async (directory) => {
             instance.chat.set(plugin.command, plugin);
           }
           instance.plugins.set(dir, plugin);
-        } 
-        catch(e) {
-          pluginsTable.addRow(plugin.name, 'NOT LOADED');
+        } catch (e) {
+          console.error(
+            `Error Attempting to Load Plugin(${dir}) located at: ${path.join(directory, dir)}`,
+          );
         }
       }
     }
     console.log(pluginsTable.toString());
   });
-}
-
+};
 
 Plugins.prototype.getSettings = (plugin) => {
-    const data = instance.plugins.get(plugin);
-    if (!data) {
-      console.error(`Invalid Plugin Requested. Check your spelling for Plugin: ${plugin}`);
-      return undefined;
-    }
-    else return plugin.settings;
-}
+  const data = instance.plugins.get(plugin);
+  if (!data) {
+    console.error(`Invalid Plugin Requested. Check your spelling for Plugin: ${plugin}`);
+    return undefined;
+  }
+  return plugin.settings;
+};
 
 Plugins.prototype.getPlugin = (plugin) => {
-    const data = instance.plugins.get(plugin);
-    if (!data) {
-      return null;
-    }
-    else return plugin;
-}
+  const data = instance.plugins.get(plugin);
+  if (!data) {
+    return null;
+  }
+  return plugin;
+};
 
 Plugins.prototype.triggerEvents = async (type, client, data, plugins) => {
-    for (var plugin of instance.plugins.entries()) {
-      var value = plugin[1];
-      if (!value.event) return;
-      if (value.type != type) return;
-      value.execute(data, client, plugins);
-    }
-}
+  for (const plugin of instance.plugins.entries()) {
+    const value = plugin[1];
+    if (!value.event) return;
+    if (value.type !== type) return;
+    value.execute(data, client, plugins);
+  }
+};
 
 Plugins.prototype.getChatCommand = (command) => {
   const data = instance.chat.get(command);
   return data;
-}
+};
 
-module.exports = function() {
-  return instance || (instance = new Plugins());
-}();
+module.exports = (() => {
+  if (!instance) instance = new Plugins();
+  return instance;
+})();
