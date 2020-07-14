86a94a4ac3208d02d3ea187ef5906803553102cc
albertoparras-dev <66566819+albertoparras-dev@users.noreply.github.com>
2020-07-14 02:08:28 +0200

    Add Modules Table

--- modules/Modules.js
+++ modules/Modules.js
@@ -1,70 +1,66 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs'), path = require('path'), util = require('util');
 
-var instance = null; // eslint-disable-line no-var
+var instance = null;
+
+var AsciiTable = require('ascii-table')
 
 function Modules() {
-  if (arguments.callee._singletonInstance) {
+  if(arguments.callee._singletonInstance) {
     return arguments.callee._singletonInstance;
   }
   arguments.callee._singletonInstance = this;
   this.modules = new Map();
 }
 
-const getDirectories = (source) =>
-  fs
-    .readdirSync(source, { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name);
+const getDirectories = source => fs.readdirSync(source, { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name)
+
+let modulesTable = new AsciiTable('MODULES');
+modulesTable.setHeading('MODULE', 'STATE')
 
 Modules.prototype.loadModules = async (directory) => {
-  fs.access(directory, (err) => {
-    if (err) throw new Error(`Folder does not Exist or has Access Permission Issues: ${directory}`);
+  fs.access(directory, function(err) {
+    if (err) new Error(`Folder does not Exist or has Access Permission Issues: ${directory}`);
     else {
       const directories = getDirectories(directory);
-      for (const dir of directories) {
+      for(const dir of directories) {
         try {
-          const module = require(path.join(directory, dir, `${dir}.js`));
-          module.settings = require(path.join(directory, dir, `${dir}.json`));
-          console.log(`Loaded Module[${module.name}] from File (/modules/${dir}/${dir}.js)`);
-          console.log(
-            `Loaded Module Settings[${module.name}] from File (/modules/${dir}/${dir}.json)`,
-          );
-          if (module.settings.active) {
-            module.activate();
-            console.log(`Actived Module[${module.name}]`);
-          }
-          instance.modules.set(dir, module);
-        } catch (e) {
-          console.error(
-            `Error Attempting to Load Module(${dir}) located at: ${path.join(directory, dir)}`,
-          );
+          var module = require(path.join(directory, dir, `${dir}.js`));
+            module.settings = require(path.join(directory, dir, `${dir}.json`));
+            modulesTable.addRow(module.name, 'LOADED');
+            if (module.settings.active) {
+              module.activate();
+              console.log(`Actived Module[${module.name}]`);
+            }
+            instance.modules.set(dir, module);
+
+        } catch(e) {
+          console.error(`Error Attempting to Load Module(${dir}) located at: ${path.join(directory, dir)}`);
           console.error(e);
         }
       }
     }
+    console.log(modulesTable.toString());
   });
-};
+}
 
 Modules.prototype.getModulesOutput = () => {
-  const data = {};
-  for (const module of instance.modules.entries()) {
-    const value = module[1];
-    if (!value.settings.active) return null;
+  var data = {};
+  for (var module of instance.modules.entries()) {
+    var value = module[1];
+    if (!value.settings.active) return;
     data[value.varname] = value.output;
   }
   return data;
-};
+}
 
 Modules.prototype.getModule = (module) => {
-  const data = instance.modules.get(module);
-  if (!data) {
-    return null;
-  }
-  return module;
-};
+    const data = instance.modules.get(module);
+    if (!data) {
+      return null;
+    }
+    else return module;
+}
 
-module.exports = (() => {
-  if (!instance) instance = new Modules();
-  return instance;
-})();
+module.exports = function() {
+  return instance || (instance = new Modules());
+}();
