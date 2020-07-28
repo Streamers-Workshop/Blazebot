114517430efba60e5f1fc76047fc8baa60f03623
krammy <keenandavidmoloney@outlook.com>
2020-07-28 02:07:42 +0100

    Added obs group toggle to alerts

--- plugins/alert-follow/alert-follow.js
+++ plugins/alert-follow/alert-follow.js
@@ -1,5 +1,25 @@
 const fs = require('fs');
 const settings = require('./alert-follow.json');
+const Modules = require('../../modules/Services.js');
+
+function toggleSource(obs) {
+  const tobj = {
+    item: { name: settings.source },
+    visible: true,
+  };
+
+  obs.send('SetSceneItemProperties', tobj).then(() => {
+    setTimeout(function a() {
+      tobj.visible = false;
+      obs
+        .send('SetSceneItemProperties', tobj)
+        .then(() => {})
+        .catch((e) => {
+          console.log(e);
+        });
+    }, settings.delay * 1000);
+  });
+}
 
 module.exports = {
   name: 'alert-follow', // Name of the Plugin
@@ -8,13 +28,13 @@ module.exports = {
   chat: false, // Defines this as a Chat Command
   event: true, // Is this a Event?
   type: 5003, // Type Event
-  command: '', // This is the Command that is typed into Chat!
+  command: 'follow', // This is the Command that is typed into Chat!
   permissions: [], // This is for Permissisons depending on the Platform.
   alias: [], // Alias commands that preform interesting things.
   cooldown: 0, // this is Set in Seconds, how long between the next usage of this command.
   settings: false, // Defining this as false will load the Settings file for this Plugin when the system loads this plugin.
   credits: `New follow system by Krammy. Original by Bioblaze Payne.`, // MAKE SURE YOU FILL THIS IN GOD DAMNIT!
-  execute(client, data) {
+  execute(client, data, modules) {
     if (settings.active) client.sendMessage(`Thanks @${data.user} for the Follow <3`);
 
     fs.writeFile('./labels/latest-follow.txt', data.user, (err) => {
@@ -23,5 +43,8 @@ module.exports = {
       }
       return true;
     });
+
+    const obs = Modules.getService('obs-controller-module');
+    if (obs) toggleSource(modules.obs);
   },
 };
--- plugins/drpc-viewercount-update/drpc-viewercount-update.js
+++ plugins/drpc-viewercount-update/drpc-viewercount-update.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const Services = require('../../modules/Services.js');
 
 module.exports = {
