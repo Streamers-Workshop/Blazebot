a8601ba9f2afe7a5d447fe617a9b05ae7c427482
krammy <keenandavidmoloney@outlook.com>
2020-08-03 02:59:46 +0100

    Added Slobs toggle support to alerts

--- plugins/alert-spell/alert-spell.js
+++ plugins/alert-spell/alert-spell.js
@@ -25,18 +25,16 @@ module.exports = {
   name: 'alert-spell', // Name of the Plugin
   description:
     'Sends a message to chat of new Spell. Saves latest spell caster to text file for obs&slobs.', // Description
-  chat: false, // Defines this as a Chat Command
-  event: true, // Is this a Event?
+  chat: true, // Defines this as a Chat Command
+  event: false, // Is this a Event?
   type: 5, // Type Event
-  command: '', // This is the Command that is typed into Chat!
+  command: 'spell', // This is the Command that is typed into Chat!
   permissions: [], // This is for Permissisons depending on the Platform.
   alias: [], // Alias commands that preform interesting things.
   cooldown: 0, // this is Set in Seconds, how long between the next usage of this command.
   settings: false, // Defining this as false will load the Settings file for this Plugin when the system loads this plugin.
   credits: `New Spell system by Krammy. Original by Bioblaze Payne.`, // MAKE SURE YOU FILL THIS IN GOD DAMNIT!
   execute(client, data, modules) {
-    console.log(data);
-
     if (settings.active) client.sendMessage(`Thanks @${data.user} for the casting a spell <3`);
 
     fs.writeFile('./labels/latest-spell.txt', data.user, (err) => {
@@ -47,6 +45,12 @@ module.exports = {
     });
 
     const obs = Modules.getService('obs-controller-module');
-    if (obs) toggleSource(modules.obs);
+    if (obs.settings.active) toggleSource(modules.obs);
+	
+	const slobs = Modules.getService('slobs-controller-module');
+	if (slobs.settings.active){
+		slobs.output.toggleSource(null, settings.source);
+		setTimeout(function a(){ slobs.output.toggleSource(null, settings.source); }, settings.delay * 1000);
+	}
   },
 };
