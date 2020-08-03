4de8229d57b9da5f23b9b578f2525d00ba82a8ac
krammy <keenandavidmoloney@outlook.com>
2020-08-03 03:01:53 +0100

    eslint

--- services/slobs/slobs.js
+++ services/slobs/slobs.js
@@ -1,10 +1,10 @@
 const SlobsJS = require('slobs.js');
 const settings = require('./slobs.json');
+
 let slobs = null;
 
-if (settings.active)
-{
-	slobs = new SlobsJS(settings.ip, settings.token);
+if (settings.active) {
+  slobs = new SlobsJS(settings.ip, settings.token);
 }
 module.exports = {
   name: 'slobs-controller-module',
