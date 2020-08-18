f0b96302e4dd6542b9f85c69bf244fea2387ffed
bulllox <56167766+bulllox@users.noreply.github.com>
2020-08-18 23:14:11 +0200

    Plug-in: Poll Alert-Spell

--- plugins/alert-spell/alert-spell.js
+++ plugins/alert-spell/alert-spell.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const settings = require('./alert-spell.json');
+const spell = require('./spells.json');
 const Bot = require('../../modules/Bot.js');
 const path = require('path');
 
-function toggleSource(obs) {
+
+function toggleSource(obs, scene, source, delay) {
   const tobj = {
-    'scene-name': settings.scene,
-    item: { name: settings.source },
+    'scene-name': scene,
+    item: { name: source },
     visible: true,
   };
 
@@ -15,11 +17,11 @@ function toggleSource(obs) {
       tobj.visible = false;
       obs
         .send('SetSceneItemProperties', tobj)
-        .then(() => {})
+        .then(() => { })
         .catch((e) => {
           Bot.log(e);
         });
-    }, settings.delay * 1000);
+    }, delay * 1000);
   });
 }
 
@@ -37,7 +39,8 @@ module.exports = {
   settings: false, // Defining this as false will load the Settings file for this Plugin when the system loads this plugin.
   credits: `New Spell system by Krammy. Original by Bioblaze Payne.`, // MAKE SURE YOU FILL THIS IN GOD DAMNIT!
   execute(client, data) {
-    if (settings.active) client.sendMessage(`Thanks @${data.user} for the casting a spell <3`);
+    Bot.log(data);
+    if (settings.active) 
 
     fs.writeFile(path.join(Bot.root, 'labels', 'latest-spell.txt'), data.user, (err) => {
       if (err) {
@@ -45,29 +48,54 @@ module.exports = {
       }
     });
 
+    var scene = "";
+    var source = "";
+    if (settings.useSpells == true) {
+      var spells = spell.spells;
 
-	//OBS SETTINGS
+      if (spell.spellTest == "") {
+        if (data['content'].name == undefined) {
+          console.log("Spell needs to have a name");
+          return false;
+        }
+        scene = spells[data['content'].name].scene;
+        source = spells[data['content'].name].source;
+        delay = parseInt(spells[data['content'].name].delay);
+      } else {
+        scene = spells[spell.spellTest].scene;
+        source = spells[spell.spellTest].source;
+        delay = parseInt(spells[spell.spellTest].delay);
+
+      }
+
+    } else {
+      scene = settings.scene;
+      source = settings.source;
+      delay = settings.delay;
+    }
+
+    //OBS SETTINGS
     const obs = Bot.getService('obs-controller-module');
-    if (obs.settings.active) toggleSource(obs.output);
+    if (obs.settings.active) toggleSource(obs.output, scene, source, delay);
 
-	//SLOBS SETTINGS
+    //SLOBS SETTINGS
     const slobs = Bot.getService('slobs-controller-module');
     if (slobs.settings.active) {
-      slobs.output.toggleSource(null, settings.source);
+      slobs.output.toggleSource(null, source);
       setTimeout(function a() {
-        slobs.output.toggleSource(null, settings.source);
-      }, settings.delay * 1000);
+        slobs.output.toggleSource(null, source);
+      }, delay * 1000);
     }
-	
-	//HTTP SETTINGS
-	var service = Bot.getService('http-overlay-module');
-	if (service){
-		service.output.notifyAll({
-		  type: "text",
-		  page: "spell",
-		  name: data.user,
-		  message: "has casted a spell!"
-		});
-	}
+
+    //HTTP SETTINGS
+    var service = Bot.getService('http-overlay-module');
+    if (service) {
+      service.output.notifyAll({
+        type: "text",
+        page: "spell",
+        name: data.user,
+        message: "has casted a spell!"
+      });
+    }
   },
 };
