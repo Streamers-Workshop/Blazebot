535e5de2c1b0f51101815d71e0d44cf60f403e7c
bulllox <bullloxstreaming@web.de>
2020-09-07 16:24:42 +0200

    pull: Fix and Sperated Spells
    Fixed obs triggering even when not using obs.
    
    Seperated the Spell Alert so if active the user can Test and Trigger Each Spell seperatly.
    
    The spells.json will be loaded as the Alert is triggered what allows user to test the Alerts on the fly without restarting the Bot

--- plugins/alerts/alerts.js
+++ plugins/alerts/alerts.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const { type } = require('os');
 
+const obsSetting = require("../../services/obs/obs.json");
+const slobsSettting = require("../../services/slobs/slobs.json");
 var settings = null;
 var followCount = 0;
 var subCount = 0;
@@ -13,52 +15,52 @@ const SUBS = 5001;
 const JOINED = 5004;
 const SPELL = 5;
 
-function write2File(fileName, data)
-{
-    if (typeof(data) !== "string")
-    {
-        data = data.toString();
+function write2File(fileName, data) {
+  if (typeof (data) !== "string") {
+    data = data.toString();
+  }
+  fs.writeFileSync(path.join(Bot.root, 'labels', fileName), data, (err) => {
+    if (err) {
+      Bot.log(Bot.translate("plugins.alerts.error_writing", {
+        fileName: fileName,
+        error: err
+      }));
     }
-    fs.writeFileSync(path.join(Bot.root, 'labels', fileName), data, (err) => {
-        if (err) {
-          Bot.log(Bot.translate("plugins.alerts.error_writing", {
-            fileName: fileName,
-            error: err
-          }));
-        }
-      });
+  });
 }
 
-function obsToggle(scene, source, delay)
-{
-    const obs = Bot.getService('obs-controller');
+function obsToggle(scene, source, delay) {
+  const obs = Bot.getService('obs-controller');
+  if (obsSetting.active) {
     if (obs.output() !== null)
-        obs.toggleSource(scene,source);
-        setTimeout(function a() {obs.toggleSource(scene,source); }
-                                                            , delay * 1000);
+      obs.toggleSource(scene, source);
+    setTimeout(function a() { obs.toggleSource(scene, source); }
+      , delay * 1000);
+  }
 
 }
-function slobsToggle(source, delay)
-{
-    const slobs = Bot.getService('slobs-controller').output();
-    if (slobs !== null) {
+function slobsToggle(source, delay) {
+  const slobs = Bot.getService('slobs-controller').output();
+  if (slobsSettting.active) {
+    if(slobs !== null) {
       slobs.toggleSource(null, source);
-      setTimeout(function a() {
+      Bot.log(`Hiding after ${delay} seconds`)
+      setTimeout(() => {
         slobs.toggleSource(null, source);
       }, delay * 1000);
     }
+  }
 }
-function https(_page, _user, _message)
-{
-    const service = Bot.getService('http-overlay-controller');
-	if (service){
-		service.output().notifyAll({
-		  type: "text",
-		  page: _page,
-		  name: _user,
-		  message: _message
-		});
-	}
+function https(_page, _user, _message) {
+  const service = Bot.getService('http-overlay-controller');
+  if (service) {
+    service.output().notifyAll({
+      type: "text",
+      page: _page,
+      name: _user,
+      message: _message
+    });
+  }
 }
 
 module.exports = {
@@ -67,16 +69,15 @@ module.exports = {
   author: "Made by Krammy",
   license: "Apache-2.0",
   permissions: ['creator'], // This is for Permissisons depending on the Platform.
-  event: [FOLLOW,SUBS,JOINED,SPELL], // Type Event
+  event: [FOLLOW, SUBS, JOINED, SPELL], // Type Event
   command: 'alerts', // This is the Command that is typed into Chat!
   cooldown: 10, // this is Set in Seconds, how long between the next usage of this command.
   execute(client, data) {
     Bot.log("Triggering");
-   
+
     if ((data.chatType === FOLLOW ||
-         (data.args !== undefined && data.args[0] === "follow" && settings.test))  
-         && settings.alerts.follow.active)
-    {
+      (data.args !== undefined && data.args[0] === "follow" && settings.test))
+      && settings.alerts.follow.active) {
       Bot.log("Following");
       ++followCount;
       write2File("latest-follow.txt", data.user);
@@ -93,10 +94,9 @@ module.exports = {
       slobsToggle(source, delay);
       https("follow", data.user, settings.alerts.follow.httpMessage);
     }
-    else if ((data.chatType === SUBS || 
-              (data.args !== undefined && data.args[0] === "sub"  && settings.test)) && 
-              settings.alerts.sub.active)
-    {
+    else if ((data.chatType === SUBS ||
+      (data.args !== undefined && data.args[0] === "sub" && settings.test)) &&
+      settings.alerts.sub.active) {
       ++subCount;
       write2File("latest-sub.txt", data.user);
       write2File("sub-count.txt", subCount);
@@ -113,15 +113,13 @@ module.exports = {
       https("sub", data.user, settings.alerts.sub.httpMessage);
     }
     else if ((data.chatType === JOINED ||
-             (data.args !== undefined && data.args[0] === "joined"  && settings.test)) && 
-             settings.alerts.joined.active)
-    {
-      write2File("latest-join.txt" , data.user);
-      if (data['live.viewers' !== undefined])
-      {
-        write2File("view-count.txt",  data['live.viewers']);
+      (data.args !== undefined && data.args[0] === "joined" && settings.test)) &&
+      settings.alerts.joined.active) {
+      write2File("latest-join.txt", data.user);
+      if (data['live.viewers' !== undefined]) {
+        write2File("view-count.txt", data['live.viewers']);
       }
-      
+
       var scene = settings.alerts.joined.scene;
       var source = settings.alerts.joined.source;
       var delay = settings.alerts.joined.delay;
@@ -134,16 +132,53 @@ module.exports = {
       slobsToggle(source, delay);
       https("joined", data.user, settings.alerts.joined.httpMessage);
     }
-    else if ((data.chatType === SPELL || 
-            (data.args !== undefined && data.args[0] === "spell" && settings.test)) 
-            && settings.alerts.spell.active)
-    {
+    else if ((data.chatType === SPELL ||
+      (data.args !== undefined && data.args[0] === "spell" && settings.test))
+      && settings.alerts.spell.active) {
       Bot.log("activating");
       write2File("latest-spell.txt", data.user);
-      var scene = settings.alerts.spell.scene;
-      var source = settings.alerts.spell.source;
-      var delay = settings.alerts.spell.delay;
-      var message = settings.alerts.spell.message;
+      const spellSettings = JSON.parse(fs.readFileSync(path.join(__dirname, 'spells.json'), "utf8"));
+      var scene = "";
+      var source = "";
+      var delay = "";
+      var message = "";
+      if (spellSettings.active) {
+        if (spellSettings.spelltest) {
+          var spellname = spellSettings.testspellName;
+          
+          if (spellSettings.spells.hasOwnProperty(spellname)) {
+            scene = spellSettings.spells[spellname].scene;
+            source = spellSettings.spells[spellname].source;
+            delay = spellSettings.spells[spellname].delay;
+            message = spellSettings.spells[spellname].message;
+          } else {
+            Bot.log(`Cant find ${spellname} triggering default spell`);
+            scene = settings.alerts.spell.scene;
+            source = settings.alerts.spell.source;
+            delay = settings.alerts.spell.delay;
+            message = settings.alerts.spell.message;
+          }
+        } else {
+          var spellName = data['content'].name;
+          if (spellSettings.spells.hasOwnProperty(spellName)) {
+            scene = spellSettings.spells[spellName].scene;
+            source = spellSettings.spells[spellName].source;
+            delay = spellSettings.spells[spellName].delay;
+            message = spellSettings.spells[spellName].message;
+          } else {
+            scene = settings.alerts.spell.scene;
+            source = settings.alerts.spell.source;
+            delay = settings.alerts.spell.delay;
+            message = settings.alerts.spell.message;
+          }
+        }
+      } else {
+        scene = settings.alerts.spell.scene;
+        source = settings.alerts.spell.source;
+        delay = settings.alerts.spell.delay;
+        message = settings.alerts.spell.message;
+      }
+      Bot.log(scene);
       var template = Handlebars.compile(message);
       client.sendMessage(template({
         user: data.user,
