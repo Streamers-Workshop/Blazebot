c1401bb52b2bde7b39729f986fbeaedf653d24a1
Butterstick822 <72819795+Butterstick822@users.noreply.github.com>
2020-10-29 22:55:50 -0400

    Fixed a Few Typos

--- tools/windows-installer.ps1
+++ tools/windows-installer.ps1
@@ -1,9 +1,9 @@
-if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
- if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-  Exit
- }
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+    $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+    exit
+  }
 }
 
 
@@ -13,10 +13,10 @@ Write-Host "Setting up Temp Directory...."
 
 Write-Host "Downloading Node.js......"
 
-if ((Get-WmiObject win32_operatingsystem | select osarchitecture).osarchitecture -like "64*"){
-  Invoke-WebRequest -Uri "https://nodejs.org/dist/v12.18.2/node-v12.18.2-x64.msi" -OutFile "$($Env:temp)\trovobot_setup\nodejs.msi"
+if ((Get-WmiObject win32_operatingsystem | Select-Object osarchitecture).osarchitecture -like "64*") {
+  Invoke-WebRequest -Uri "https://nodejs.org/dist/latest-fermium/node-v14.15.0-x64.msi" -OutFile "$($Env:temp)\trovobot_setup\nodejs.msi"
 } else {
-  Invoke-WebRequest -Uri "https://nodejs.org/dist/v12.18.2/node-v12.18.2-x86.msi" -OutFile "$($Env:temp)\trovobot_setup\nodejs.msi"
+  Invoke-WebRequest -Uri "https://nodejs.org/dist/latest-fermium/node-v14.15.0-x86.msi" -OutFile "$($Env:temp)\trovobot_setup\nodejs.msi"
 }
 
 Write-Host "Download Complete! Now Installing Node.js.........."
@@ -37,11 +37,11 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 
 Write-Host "Installing Deps via Chocolatey...."
 
-iex "choco upgrade -y python visualstudio2019-workload-vctools yarn"
+Invoke-Expression "choco upgrade -y python visualstudio2019-workload-vctools yarn"
 
 Write-Host "Refreshing env...."
 
-iex "refreshenv"
+Invoke-Expression "refreshenv"
 
 Write-Host "Downloading TrovoBot..."
 
@@ -51,7 +51,7 @@ Write-Host "Setting up TrovoBot..."
 
 Expand-Archive -Path "$($Env:temp)\trovobot_setup\master.zip" -DestinationPath "$($PSCommandPath[0]):\" -Verbose
 
-Rename-Item -path "$($PSCommandPath[0]):\TrovoBot-master" -newName "$($PSCommandPath[0]):\TrovoBot"
+Rename-Item -Path "$($PSCommandPath[0]):\TrovoBot-master" -NewName "$($PSCommandPath[0]):\TrovoBot"
 
 Start-Process -Wait -NoNewWindow 'yarn' -ArgumentList 'install' -WorkingDirectory "$($PSCommandPath[0]):\TrovoBot"
 
