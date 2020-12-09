1696f95f99eca5aafd77be62fac645607318b365
Praxem <53101345+PraxemAN@users.noreply.github.com>
2020-12-09 11:33:20 -0600

    Plugin added
    Adjusted some more READMEs and added the gamble plugin within' user-info processor.

--- plugins/timed-messages/README.md
+++ plugins/timed-messages/README.md
@@ -5,9 +5,9 @@ created by Wikinger1988
 Enable automatic timed messages
 
 ## Trigger:
-!timers start to start the auto messages
-!timers stop to stop the auto messages
+!timers start // to start the auto messages
+!timers stop // to stop the auto messages
 
 ## Requirements:
-Edit `messages.txt` file with your personal messages.
+Edit `messages.txt` file with your personal messages. Each line is a message.
 Edit `timed-messages.json` file with a specific time you want to be reminded (default is 20 mins).
---
+++ processors/user-info/plugins/gamble/gamble.js
@@ -0,0 +1,73 @@
+const Bot = require('../../../../modules/Bot.js');
+var path = require('path');
+var userInfo = null;
+
+module.exports = {
+  name: 'gamble', // Name of the Plugin
+  description: "Takes a given amount of points from user to gamble them",
+  author: "Krammy:Source, Praxem:Mutation into gamble",
+  license: "Apache-2.0",
+  command: 'gamble', // This is the Command that is typed into Chat!
+  permissions: [], // This is for Permissisons depending on the Platform.
+  cooldown: 1, // this is Set in Seconds, how long between the next usage of this command.
+  execute(client, data) {
+    if (data.args.length > 0)
+    {
+		//Is capable of gambling
+        if (userInfo[data.user].points > 0)
+        {
+			var bet = 0;
+			if (data.args[0] === `all`) { data.args[0] = (userInfo[data.user].points).toString(); }
+			
+			//Is capable of gambling with declared points
+			if(userInfo[data.user].points >= parseInt(data.args[0]) && parseInt(data.args[0]) > 0) 
+			{
+				if(bet == 0) { bet = parseInt(data.args[0]); }
+				
+				//remove bet from user
+				userInfo[data.user].points = userInfo[data.user].points - bet;
+				
+				
+				//outcome of the bet //best is 4 times //worst is complete loss of bet (12.5% chance to win)
+				var result = ( Math.random() - Math.random() ); //min, max                           
+				
+				//limit to just the bet
+				if ( (result) <= 0.51) { client.sendMessage(`${data.user}, you lost your bet of ${bet}. You have, ${userInfo[data.user].points} points left.`); }
+				else if (result > 0.51) //12.5% chance to win
+				{
+				//combining outcomes		
+				userInfo[data.user].points = userInfo[data.user].points + (bet * 3);
+				
+				client.sendMessage(`Congratulations ${data.user}, you won: ${bet * 3}. Current points: ${userInfo[data.user].points}`);
+				}			
+			}
+			else if (userInfo[data.user].points < parseInt(data.args[0]) && parseInt(data.args[0]) > 0) 
+			{
+				//user doesn't have enough points as their intended bet
+				client.sendMessage(`${data.user}, you do not have enough points.`);	
+			}
+			//if the bet is a negative number or 0
+			else if (parseInt(data.args[0]) < 1) 
+			{
+				client.sendMessage(`${data.user}, you need to be above 0 points.`);	
+			}
+			 else
+			{
+				client.sendMessage(Bot.translate("processor.user_info.plugins.gamble.error_user"));
+			}			       
+        }
+		 else
+			{
+				client.sendMessage(`You need more points to gamble.`);
+			}	
+    }
+  },
+  activate() {
+    userInfo = require(path.join(Bot.data, "users/users.json"));
+    Bot.log(Bot.translate("processors.user_info.plugins.give.activated"))
+  },
+  deactivate() {
+    userInfo = null;
+    Bot.log(Bot.translate("processors.user_info.plugins.give.deactivated"))
+  }
+};
